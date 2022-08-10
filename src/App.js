29f66480a4e558aa91f140652eeda254061b81b8
Ik-23 <irfank0223@gmail.com>
2022-08-10 14:50:38 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Academica from './component/Basics/Academica'
+
+const App = () => {
+  return (
+    <div>
+      <Academica/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/component/Basics/menuApi.js
@@ -0,0 +1,85 @@
+const Menu = [
+  {
+    // id: 1,
+    image: "images/C.png",
+    name: "C",
+    category: "1st sem",
+    description:
+      "C is a general-purpose computer programming language.",
+  },
+
+  {
+    // id: 2,
+    image: "images/c++.png",
+    name: "C++",
+    category: "2nd sem",
+    description:
+      "C++ is a popular programming language. C++ is used to create computer programs, and is one of the most used language in game development.",
+  },
+  {
+    // id: 3,
+    image: "images/Java.png",
+    name: "Java",
+    category: "3rd sem",
+    description:
+      "Java is a popular programming language. Java is used to develop mobile apps, web apps, desktop apps, games and much more.",
+  },
+  {
+    // id: 4,
+    image: "images/js.png",
+    name: "JavaScript",
+    category: "3rd sem",
+    description:
+      "JavaScript is the world's most popular programming language. JavaScript is the programming language of the Web.",
+  },
+  {
+    // id: 5,
+    image: "images/Python.png",
+    name: "Python",
+    category: "4th sem",
+    description:
+      "Python can be used on a server to create web applications and it's convenience has made it the most popular language for machine learning and artificial intelligence.",
+  },
+  {
+    // id: 6,
+    image: "images/sql.png",
+    name: "SQL",
+    category: "4th sem",
+    description:
+      "SQL is a standard language for storing, manipulating and retrieving data in databases.",
+  },
+  {
+    // id: 7,
+    image: "images/React.png",
+    name: "React.js",
+    category: "5tm sem",
+    description:
+      "React is a JavaScript library for building user interfaces. React is used to build single-page applications.",
+  },
+  {
+    // id: 8,
+    image: "images/nodejs.png",
+    name: "Node.js",
+    category: "5tm sem",
+    description:
+      "Node.js is an open-source, cross-platform, back-end JavaScript runtime environment that runs on the V8 engine and executes JavaScript code outside a web browser, which was designed to build scalable network applications.",
+  },
+  {
+    // id: 9,
+    image: "images/mongodb.png",
+    name: "Mongodb",
+    category: "5tm sem",
+    description:
+      "MongoDB is a source-available cross-platform document-oriented database program. Classified as a NoSQL database program, MongoDB uses JSON-like documents with optional schemas.",
+  },
+  {
+    // id: 9,
+    image: "images/expressjs.png",
+    name: "Express.js",
+    category: "5tm sem",
+    description:
+      "Express.js, or simply Express, is a back end web application framework for Node.js.",
+  },
+];
+
+export default Menu;
